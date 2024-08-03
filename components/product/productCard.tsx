568e9b9e8913875de5b489e9a738775c284b506e
Sahra <sahra@Sahra-Mac.local>
2024-08-03 10:19:26 +0330

    add rating component

--- components/product/productCard.tsx
+++ components/product/productCard.tsx
@@ -1,6 +1,7 @@
 import { Product } from "@/lib/models/productModel";
 import Image from "next/image";
 import Link from "next/link";
+import { Rating } from "./Rating";
 
 export default function ProductCard({ product }: { product: Product }) {
   return (
@@ -20,6 +21,7 @@ export default function ProductCard({ product }: { product: Product }) {
         <Link href={`/product/${product.slug}`}>
           <h2 className="text-2xl font-normal">{product.name}</h2>
         </Link>
+        <Rating value={product.rating} caption={`(${product.numReviews})`} />
         <p className="mb-2">{product.brand}</p>
         <div className="card-actions flex items-center justify-between">
           <span className="text-2xl">${product.price}</span>
